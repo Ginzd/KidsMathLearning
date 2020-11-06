6e9c2b3ef77e62b53d45aee036248b9246d7bd5c
Huy <huypcph11075@fpt.edu.vn>
2020-11-06 14:23:04 +0700

    Moi nhat

--- app/src/main/java/com/example/kidsmathlearning/MainActivity.java
+++ app/src/main/java/com/example/kidsmathlearning/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "ÁHDGAS", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Code by huy", Toast.LENGTH_SHORT).show();
     }
 }
