51b591d02646a9ce89e394bd4b1bf61832333340
nguyenhieu21102001 <59793171+nguyenhieu21102001@users.noreply.github.com>
2020-11-06 15:28:41 +0700

    hieu123
    test

--- app/src/main/java/com/example/kidsmathlearning/MainActivity.java
+++ app/src/main/java/com/example/kidsmathlearning/MainActivity.java
@@ -1,16 +1,17 @@
 package com.example.kidsmathlearning;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "ÁHDGAS", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hiếu", Toast.LENGTH_SHORT).show();
     }
 }
