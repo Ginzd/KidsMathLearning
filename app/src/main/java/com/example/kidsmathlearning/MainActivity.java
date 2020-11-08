0867cf86d587664afd7e2b2fd15674d739f43d88
gind <ginzmod@gmail.com>
2020-11-08 17:00:15 +0700

    Main codeddddd

--- app/src/main/java/com/example/kidsmathlearning/MainActivity.java
+++ app/src/main/java/com/example/kidsmathlearning/MainActivity.java
@@ -26,10 +26,10 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Hiếu", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "Hiếu", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "Hiếu", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "Hiếu", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Master", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Master", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Master", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Master", Toast.LENGTH_SHORT).show();
             }
         });
     }
