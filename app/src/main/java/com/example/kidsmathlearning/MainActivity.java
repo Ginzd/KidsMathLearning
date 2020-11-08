0d11d14b5f05eeafcb99740c07c7025fc44d7452
nguyenhieu21102001 <59793171+nguyenhieu21102001@users.noreply.github.com>
2020-11-08 11:05:03 +0700

    08_11_2020
    ManHinhChao

--- app/src/main/java/com/example/kidsmathlearning/MainActivity.java
+++ app/src/main/java/com/example/kidsmathlearning/MainActivity.java
@@ -3,17 +3,47 @@ package com.example.kidsmathlearning;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imgLogo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "ÁHDGAS", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "hiếu", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "ÁHDGAS", Toast.LENGTH_SHORT).show();
 
+
+        Button button = findViewById(R.id.button);
+        MyRunnable myRunnable = new MyRunnable();
+        myRunnable.start();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+    private class MyRunnable extends Thread {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(2000);
+                imgLogo = findViewById(R.id.imageViewlogo);
+                Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.demo);
+                imgLogo.startAnimation(animation);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
