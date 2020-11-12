2d9ad17b3a1a138c5634a5f237e525d2038a2ad2
nguyenhieu21102001 <59793171+nguyenhieu21102001@users.noreply.github.com>
2020-11-12 14:11:17 +0700

    hiue
    hiue

--- app/src/main/java/com/example/kidsmathlearning/MainActivity.java
+++ app/src/main/java/com/example/kidsmathlearning/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -14,27 +15,47 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imgLogo;
+    ImageView imgLogo, imgNhacNen;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.button);
+        imgNhacNen = findViewById(R.id.imageView2);
+        mediaPlayer = MediaPlayer.create(this, R.raw.nhacnen);
         MyRunnable myRunnable = new MyRunnable();
         myRunnable.start();
+
+        imgNhacNen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pauseAction();
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Hiếu", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "Hiếu", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "Hiếu", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "Hiếu", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "Hiếu", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "Hiếu ngu vcl", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, MainActivity_Home.class);
+                startActivity(intent);
             }
         });
     }
+
+
+    public int PlayAction() {
+        mediaPlayer.start();
+        return 1;
+    }
+
+    public int pauseAction() {
+        mediaPlayer.pause();
+        imgNhacNen.setImageResource(R.drawable.unloa);
+        return -1;
+    }
+
+
     private class MyRunnable extends Thread {
         @Override
         public void run() {
@@ -43,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
                 imgLogo = findViewById(R.id.imageViewlogo);
                 Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.demo);
                 imgLogo.startAnimation(animation);
+                PlayAction();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
---
+++ app/src/main/java/com/example/kidsmathlearning/MainActivity_Home.java
@@ -0,0 +1,20 @@
+package com.example.kidsmathlearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity_Home extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__home);
+    }
+
+}
